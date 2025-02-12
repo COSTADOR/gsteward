b5ccfa5c2e4e041a04158445af7213fd61bba42f
Andrey Debelinsky <costandrik@gmail.com>
2025-02-12 06:43:46 -0800

    Revert "AdvancedMarkerElement"
    This reverts commit 412a7302eaa2fb5ff77e34ea9cc8c3eb26222418.

--- src/components/sections/contact/contact.tsx
+++ src/components/sections/contact/contact.tsx
@@ -1,30 +1,11 @@
-import React, { useEffect, useRef } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import "./contact.scss"
-import { GoogleMap, LoadScript } from "@react-google-maps/api"
-import {
-  center,
-  containerStyle,
-  G_API_KEY,
-  mapStyles,
-  markers,
-} from "../../../constants/map.const"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
+import { center, containerStyle, G_API_KEY, mapStyles, markers } from "../../../constants/map.const"
 import { CONTACT_INFO } from "../../../constants/contacts.const"
 
 const Contact: React.FC = () => {
-  const mapRef = useRef<google.maps.Map | null>(null)
-
-  useEffect(() => {
-    if (mapRef.current) {
-      markers.forEach(marker => {
-        new google.maps.marker.AdvancedMarkerElement({
-          map: mapRef.current!,
-          position: { lat: marker.lat, lng: marker.lng },
-        })
-      })
-    }
-  }, [])
-
   return (
     <section className="contact">
       <div className="contact__container container">
@@ -57,10 +38,14 @@ const Contact: React.FC = () => {
               center={center}
               zoom={10}
               options={{ styles: mapStyles }}
-              onLoad={map => {
-                mapRef.current = map
-              }}
-            />
+            >
+              {markers.map((marker, index) => (
+                <Marker
+                  key={index}
+                  position={{ lat: marker.lat, lng: marker.lng }}
+                />
+              ))}
+            </GoogleMap>
           </LoadScript>
         </div>
       </div>
--- src/components/sections/lets-connect/lets-connect.tsx
+++ src/components/sections/lets-connect/lets-connect.tsx
@@ -1,29 +1,10 @@
-import React, { useEffect, useRef } from "react"
+import React from "react"
 import "./lets-connect.scss"
-import { GoogleMap, LoadScript } from "@react-google-maps/api"
-import {
-  center,
-  containerStyle,
-  G_API_KEY,
-  mapStyles,
-  markers,
-} from "../../../constants/map.const"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
+import { center, containerStyle, G_API_KEY, mapStyles, markers } from "../../../constants/map.const"
 import { CONTACT_INFO } from "../../../constants/contacts.const"
 
 const LetsConnect: React.FC = () => {
-  const mapRef = useRef<google.maps.Map | null>(null)
-
-  useEffect(() => {
-    if (mapRef.current) {
-      markers.forEach(marker => {
-        new google.maps.marker.AdvancedMarkerElement({
-          map: mapRef.current!,
-          position: { lat: marker.lat, lng: marker.lng },
-        })
-      })
-    }
-  }, [])
-
   return (
     <section className="lets-connect">
       <div className="lets-connect__container container">
@@ -74,10 +55,14 @@ const LetsConnect: React.FC = () => {
               center={center}
               zoom={10}
               options={{ styles: mapStyles }}
-              onLoad={map => {
-                mapRef.current = map
-              }}
-            />
+            >
+              {markers.map((marker, index) => (
+                <Marker
+                  key={index}
+                  position={{ lat: marker.lat, lng: marker.lng }}
+                />
+              ))}
+            </GoogleMap>
           </LoadScript>
         </div>
       </div>
